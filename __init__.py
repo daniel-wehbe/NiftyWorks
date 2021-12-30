8d1041c8f4ba69bfa1736e81e9425a4553a35d8b
Daniel Wehbe <danadels99@gmail.com>
2021-12-31 01:36:46 +0200

    new code for website

---
+++ __init__.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, jsonify
+from flask.templating import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('main.html')
+
+
+if __name__ == "__main__":
+    app.run(
+        debug=True,
+        host="0.0.0.0",
+        port=7004
+    )
